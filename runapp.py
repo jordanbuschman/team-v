b546d1481770d315dadc5d17abce04940eb6594c
jordanbuschman <jordan.buschman@me.com>
2014-07-08 01:44:04 -0700

    Trying this

--- runapp.py
+++ runapp.py
@@ -1,10 +1,10 @@
-from pyramid.paster import get_app
 from socketio.server import SocketIOServer
+from paste.deply import loadapp
 
 if __name__ == "__main__":
     open('transcript.log', 'w').close() # Make sure transcript is created and empty
 
-    app = get_app('production.ini')
+    app = loadapp('config:production.ini', relative_to='.')
     SocketIOServer(('0.0.0.0', 5000), app, 
         resource="socket.io", policy_server=True,
         policy_listener=('0.0.0.0', 10843)).serve_forever()
