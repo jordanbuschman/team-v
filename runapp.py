d3e520e082c6a86d50534eef1704fe81487a7e43
jordanbuschman <jordan.buschman@me.com>
2014-07-08 00:54:47 -0700

    One last try before bed

--- runapp.py
+++ runapp.py
@@ -1,11 +1,12 @@
 import os, socket, select
 
-from paste.deploy import loadapp
+from pyramid.paster import get_app
 from socketio.server import SocketIOServer
 
 if __name__ == "__main__":
     open('transcript.log', 'w').close() # Make sure transcript is created and empty
 
-    port = int(os.environ.get("PORT", 5000))
-    app = loadapp('config:production.ini', relative_to='.')
-    SocketIOServer(('0.0.0.0', port), app, resource="socket.io").serve_forever()
+    app = get_app('production.ini')
+    SocketIOServer(('0.0.0.0', 5000), app, 
+        resource="socket.io", policy_server=True,
+        policy_listener=('0.0.0.0', 10843)).serve_forever()
