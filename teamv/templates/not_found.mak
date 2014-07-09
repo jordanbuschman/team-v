143c8d71ce7b6a1b3f0b24a68be91e2f40e86123
jordanbuschman <jordan.buschman@me.com>
2014-07-09 09:48:29 -0700

    Work done on viewing transcripts

---
+++ teamv/templates/not_found.mak
@@ -0,0 +1,4 @@
+<%inherit file="wrapper.mak" />
+
+<h2>404 - Not Found</h2>
+<p>The file you were looking for was not found</p>
--- teamv/views.py
+++ teamv/views.py
@@ -1,7 +1,7 @@
 from pyramid.view import view_config
 from pyramid.response import Response
 from pyramid.response import FileResponse
-from os import environ
+from os import environ, path
 from socketio import socketio_manage
 from socketio.namespace import BaseNamespace
 from chat import ChatNamespace
@@ -16,14 +16,24 @@ def index(request):
     result = mytemplate.render()
     return Response(result)
 
-@view_config(route_name='transcript')
+@view_config(route_name='transcript', renderer='mako', request_method='GET')
 def transcript(request):
-    response = FileResponse(
-        'transcript.log',
-        request = request,
-        content_type='text/plain'
-    )
-    return response
+    if 'meeting_id' in request.GET:
+        file_name = 'transcript_{0}.log'.format(request.GET.get('meeting_id'))
+        if os.path.isfile(file_name):
+            mytemplate = mylookup.get_template('transcript.mak')
+            result = mytemplate.render(transcript = file_name)
+            return Response(result)
+        else:
+            mytemplate = mylookup.get_template('not_found.mak')
+            request.response.status = '404 Not Found'
+            result = mytemplate.render()
+            return Response
+    else:
+        mytemplate = mylookup.get_template('not_found.mak')
+        request.response.status = '404 Not Found'
+        result = mytemplate.render()
+        return Response
 
 @view_config(route_name='socketio')
 def socketio(request):
