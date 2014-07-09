c2952e38c6f8f2b039a2b23c360bb948d84339c5
jordanbuschman <jordan.buschman@me.com>
2014-07-09 10:41:13 -0700

    Made '404 - Not Found' page

--- teamv/views.py
+++ teamv/views.py
@@ -1,14 +1,23 @@
-from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPNotFound
 from pyramid.response import Response
 from pyramid.response import FileResponse
-from os import environ, path
-from socketio import socketio_manage
+from pyramid.view import view_config
 from socketio.namespace import BaseNamespace
-from chat import ChatNamespace
-from mako.lookup import TemplateLookup
+from socketio import socketio_manage
 from mako.template import Template
+from mako.lookup import TemplateLookup
+from chat import ChatNamespace
+from os import environ, path
+
+import os
 
 mylookup = TemplateLookup(directories=['./teamv/templates'])
+
+@view_config(context=HTTPNotFound, renderer='mako')
+def not_found(request):
+    mytemplate = mylookup.get_template('not_found.mak')
+    result = mytemplate.render()
+    return Response(result, status = '404 Not Found')
 
 @view_config(route_name='home', renderer='mako')
 def index(request):
@@ -19,21 +28,15 @@ def index(request):
 @view_config(route_name='transcript', renderer='mako', request_method='GET')
 def transcript(request):
     if 'meeting_id' in request.GET:
-        file_name = 'transcript_{0}.log'.format(request.GET.get('meeting_id'))
+        file_name = 'teamv/templates/logs/transcript_{0}.mak'.format(request.GET.get('meeting_id'))
         if os.path.isfile(file_name):
             mytemplate = mylookup.get_template('transcript.mak')
-            result = mytemplate.render(transcript = file_name)
+            result = mytemplate.render(transcript_name = file_name)
             return Response(result)
         else:
-            mytemplate = mylookup.get_template('not_found.mak')
-            request.response.status = '404 Not Found'
-            result = mytemplate.render()
-            return Response
+            return not_found(request)
     else:
-        mytemplate = mylookup.get_template('not_found.mak')
-        request.response.status = '404 Not Found'
-        result = mytemplate.render()
-        return Response
+        return not_found(request)
 
 @view_config(route_name='socketio')
 def socketio(request):
