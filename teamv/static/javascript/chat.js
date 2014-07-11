51a89d8ee7cd0d2b2b5899ce198334718bf2ece0
Jose Vega <josealex380@gmail.com>
2014-07-11 13:11:38 -0700

    voice to text update

--- teamv/static/javascript/chat.js
+++ teamv/static/javascript/chat.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
     var socket = io.connect('/chat');
 
+    if (!('webkitSpeechRecognition' in window)) {
+	  upgrade();
+    } 	
+
+    var recognition = new window.webkitSpeechRecognition();
+  
+    recognition.continuous = true;
+    recognition.onresult = function(event) {
+	for(var i = event.resultIndex; i < event.results.length; i++){
+	    if(event.results.length > 0){
+	    chatbox.value = event.results[i][0].transcript;
+	    $('#chat_form').submit();
+	    }
+	}
+    };
+
+    
+   $('#stop_listening').on('click', function(){
+	recognition.stop();
+   });
+
+   $('#start_listening').on('click', function() {
+	recognition.start();
+    });
+
     $(window).bind("beforeunload", function() {
         socket.disconnect();
     });
@@ -17,10 +42,12 @@ $(document).ready(function() {
         $("#chatlog").append("user connected" + "<br />");
     });
 
+
     $("#chat_form").submit(function(e) {
         e.preventDefault();
         var val = $("#chatbox").val();
         socket.emit("chat", val);
         $("#chatbox").val("");
     });
+
 });
--- teamv/templates/index.mak
+++ teamv/templates/index.mak
@@ -7,7 +7,9 @@
 <h1>Chat Log</h1>
 <div id="chatlog"></div>
 <br />
-<form id="chat_form">
+<form name = "chat_form"  id="chat_form">
     <input type="text" id="chatbox"></input>
-    <button type="submit" id="submit">Send</button>
+    <button type="submit" id="submit" >Send</button>
+    <input type="button" id="stop_listening" value="Stop Listening" onclick="recognition.stop()">
+    <input type="button" id="start_listening" value="Start Listening" onclick="recognition.start()">
 </form>
